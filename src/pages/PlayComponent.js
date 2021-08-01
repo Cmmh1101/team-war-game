02576dd3652842601ac2497feddc0be2586ad271
illfriday <willprouty@gmail.com>
2021-08-01 00:08:51 -0700

    add logic to send cards to the winner, add quadruple war logic, start win condition

--- src/pages/PlayComponent.js
+++ src/pages/PlayComponent.js
@@ -27,6 +27,7 @@ class Play extends Component {
         playerOne: '',
         playerTwo: ''
       },
+      cardsInLimbo: [],
       buttText : 'Shuffle the cards',
       playing: false,
       showCards: false,
@@ -100,15 +101,43 @@ class Play extends Component {
     this.setState({
       showCards : true
     });
+    //shows the first draw
 
-    let p1CurrentCard = this.state.playerDeck.playerOne.shift();
-    let p2CurrentCard = this.state.playerDeck.playerTwo.shift();
-    //set current card as the first item in the playerDeck array for each player
-    let p1CurrentVal = p1CurrentCard.substring(1);
+    //checks win condition
+    //NEED TO: update win count, re-shuffle and deal, make sure state is updated correctly(reset buttons, cards)
+    if (this.state.playerDeck.playerOne.length === 0) {
+      alert('Player Two is the winner');
+    } else if (this.state.playerDeck.playerTwo.length === 0) {
+      alert('Player One is the winner');
+    }
+
+    let p1CurrentCard = '';
+    let p2CurrentCard = '';
+    let p1Arr = [...this.state.playerDeck.playerOne];
+    let p2Arr = [...this.state.playerDeck.playerTwo];
+
+    //Copy the player hands and remove the current card AVOID MUTATING STATE DIRECTLY
+    const pullCard =  () => {
+      
+      //make a SHALLOW COPY of the playerDeck ARRAYS from STATE
+      // console.log('p1Arr' + p1Arr);
+ 
+      p1CurrentCard = p1Arr.shift();
+      p2CurrentCard = p2Arr.shift();
+      //set current card as the first item in the playerDeck array for each player
+      this.setState({
+        playerDeck: { playerOne: p1Arr, playerTwo: p2Arr}
+      })
+      //update the playerDeck in the STATE - 1 card
+    }
+    pullCard();
     
+    
+    let p1CurrentVal = p1CurrentCard.substring(1);
     let p2CurrentVal = p2CurrentCard.substring(1);
     //sets the current value as the last character in the current card. returns STRING
-    console.log(p1CurrentCard, p2CurrentCard, p1CurrentVal, p2CurrentVal);
+    console.log(`p1card: ${p1CurrentCard}, p1val:${p1CurrentVal} 
+     p2card: ${p2CurrentCard}, p2val:${p2CurrentVal}`);
     
     const checkNum = (val) => {
       if (isNaN(Number(val)) ) {
@@ -126,11 +155,40 @@ class Play extends Component {
       //checks if current value is Nan. If it is converts the letter character to the appropriate number value, if not converts the value string to a number
 
       const compareVal = (v1, v2) => {
+        let cardsInPlay = [p1CurrentCard, p2CurrentCard];
+        let winningsArr = [];
+
+        console.log('inPlay', cardsInPlay);
         if (v1 === v2) {
           console.log('war');
+          cardsInPlay = cardsInPlay.concat(p1Arr.splice(0, 4), p2Arr.splice(0, 4),);
+          console.log(cardsInPlay);
+          this.setState({
+            cardsInLimbo: cardsInPlay
+          });
+          cardsInPlay = [];
         } else if (v1 > v2) {
-          console.log('p1 wins');
-        } else {console.log('p2wins');}
+          if (this.state.cardsInLimbo.length !== -1) {
+            winningsArr = [...this.state.cardsInLimbo];
+            this.setState({
+              cardsInLimbo: []
+            })
+          }
+          p1Arr = p1Arr.concat(cardsInPlay, winningsArr);
+          console.log('p1 wins', p1Arr[p1Arr.length-2], p1Arr[p1Arr.length-1], 'p1#', p1Arr.length, 'p2#', p2Arr.length);
+        } else {
+          if (this.state.cardsInLimbo.length !== -1) {
+            winningsArr = [...this.state.cardsInLimbo];
+            this.setState({
+              cardsInLimbo: []
+            })
+          };
+          p2Arr = p2Arr.concat(cardsInPlay, winningsArr);
+          console.log('p2wins', p2Arr[p2Arr.length-2], p2Arr[p2Arr.length-1], 'p1#', p1Arr.length, 'p2#', p2Arr.length);}
+        
+        this.setState({
+        playerDeck: { playerOne: p1Arr, playerTwo: p2Arr}
+      })
       }
       //accepts 2 numbers and compares. 
       //NEED TO UPDATE LOGIC TO PUSH THE CARDS TO THE APPROPRIATE PLAYER
@@ -152,7 +210,7 @@ class Play extends Component {
           default: console.log('Suit error');
         }
         path += '.png'
-        console.log(path, typeof path); 
+        // console.log(path, typeof path); 
         return path;
       }
       
