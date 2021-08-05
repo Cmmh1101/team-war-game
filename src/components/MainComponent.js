bdaee00a8a16a8cbc61329b92d0e64a9c64f8ccc
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-08-05 07:34:47 -0500

    fixed react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import Homepage from "../pages/HomepageComponent";
 import Play from "../pages/PlayComponent";
 import Rules from "../pages/RulesComponent";
 import ContactUs from "../pages/ContactUsComponent";
 import Navigation from "./NavigationComponent";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function Main() {
   return (
     <React.Fragment>
       <Navigation />
-      <Switch>
-        <Route path="/homepage" component={Homepage} />
-        <Route exact path="/play" component={Play} />
-        <Route exact path="/rules" component={Rules} />
-        <Route exact path="/contactus" component={ContactUs} />
-        <Redirect to="/homepage" />
-      </Switch>
+      <TransitionGroup>
+        <CSSTransition classNames="page" timeout={300}>
+          <Switch>
+            <Route path="/homepage" component={Homepage} />
+            <Route exact path="/play" component={Play} />
+            <Route exact path="/rules" component={Rules} />
+            <Route exact path="/contactus" component={ContactUs} />
+            <Redirect to="/homepage" />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     </React.Fragment>
   );
 }
 
-export default Main;
+export default withRouter(Main);
