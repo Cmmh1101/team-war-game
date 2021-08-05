47cfcb8887aa2890ab0be3a8c4aeb85a8d6d5507
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-08-04 23:24:45 -0500

    Added animations to homepage and rules

--- src/components/HeroComponent.js
+++ src/components/HeroComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import { FadeTransform } from "react-animation-components";
 
 function Hero() {
   return (
@@ -8,25 +9,49 @@ function Hero() {
       <Container>
         <Row>
           <Col className="d-flex justify-content-center align-items-center flex-column">
-            <h1 className="my-5 titles hero-title">Quadruple War</h1>
-            <p className="lead">
-              <strong>A React Card Game</strong>
-              <br /> by Carla Montano and Will Prouty
-            </p>
-            <Link to="/play" className="col mb-3 text-warning mb-lg-0 btn-hero">
-              PLAY <i className="fa fa-arrow-right" aria-hidden="true" />
-            </Link>
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+            >
+              <h1 className="my-5 titles hero-title">Quadruple War</h1>
+            </FadeTransform>
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+              delay="800"
+            >
+              <p className="lead">
+                <strong>A React Card Game</strong>
+                <br /> by Carla Montano and Will Prouty
+              </p>
+              <Link to="/play" className="col text-warning mb-lg-0 btn-hero">
+                PLAY <i className="fa fa-arrow-right" aria-hidden="true" />
+              </Link>
+            </FadeTransform>
           </Col>
+
           <Col lg={6}>
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/z2K6uMX_g-U"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            />
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+              delay="1000"
+            >
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/z2K6uMX_g-U"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              />
+            </FadeTransform>
           </Col>
         </Row>
       </Container>
--- src/pages/RulesComponent.js
+++ src/pages/RulesComponent.js
@@ -1,139 +1,203 @@
 import React from "react";
 import { Media } from "reactstrap";
-
-// import { FadeTransform, Fade, Stagger } from "react-animation-components";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function Rules() {
+  const items = ["first"];
   return (
     <div className="main-section">
       <div className="container text-white my-5">
-        <div className="row">
-          <div className="col">
-            <h1 className="text-center titles">Quadruple War Game Rules </h1>
+        <FadeTransform
+          in
+          transformProps={{
+            exitTransform: "scale(0.5) translateX(-100%)",
+          }}
+        >
+          <div className="row">
+            <div className="col">
+              <h1 className="text-center titles">Quadruple War Game Rules </h1>
+            </div>
           </div>
-        </div>
+        </FadeTransform>
+
         <div className="row">
           <div className="col">
-            <section>
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+            >
               <p>
                 <strong>Quadruple War</strong> is a variant of the centuries old
                 card game <strong> War</strong> played with 2 players.
               </p>
               <strong>To win, you must capture all of the cards!</strong>
-            </section>
-            <section>
+            </FadeTransform>
+
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+              delay="1000"
+            >
               <h3 className="my-2 titles">How To Play</h3>
-            </section>
-            <hr />
-            <Media>
-              <Media left className="my-auto">
-                <Media
-                  object
-                  className="rules-card mr-4 d-none d-block"
-                  src="../cards/backcard.jpg"
-                  alt="Generic placeholder image"
-                />
-              </Media>
-              <Media body>
-                <Media heading>
-                  <strong>Set up</strong>
+
+              <hr />
+            </FadeTransform>
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+              delay="2000"
+            >
+              <Media>
+                <Media left className="my-auto">
+                  <Media
+                    object
+                    className="rules-card mr-4 d-none d-block"
+                    src="../cards/backcard.jpg"
+                    alt="Generic placeholder image"
+                  />
                 </Media>
-                <p>
-                  We take a standard 52 card deck of playing cards, shuffle, and
-                  deal into 2 player decks of 26 cards. Cards are placed
-                  face-down in front of each player.
-                </p>
-              </Media>
-            </Media>
-            <hr />
-            <Media>
-              <Media left className="my-auto">
-                <Media
-                  object
-                  className="rules-card mr-4"
-                  src="../cards/backcard.jpg"
-                  alt="Generic placeholder image"
-                />
-              </Media>
-              <Media body>
-                <Media heading>
-                  <strong>Game Play</strong>
+
+                <Media body>
+                  <Media heading>
+                    <strong>Set up</strong>
+                  </Media>
+                  <p>
+                    We take a standard 52 card deck of playing cards, shuffle,
+                    and deal into 2 player decks of 26 cards. Cards are placed
+                    face-down in front of each player.
+                  </p>
                 </Media>
-                <p>
-                  Each player takes the top card of their hand and flips it
-                  face-up. The player with the highest value card, wins the
-                  hand. The card values are in numerical order from 2 to 14.
-                  Number cards reflect their values(2-10), Jacks are worth 11,
-                  Queens 12, Kings 13, and Aces high at 14. The winning player
-                  takes both cards and places them at the bottom of their player
-                  deck.
-                </p>
               </Media>
-            </Media>
-            <hr />
-            <Media>
-              <Media left className="my-auto">
-                <Media
-                  object
-                  className="rules-card mr-4"
-                  src="../cards/backcard.jpg"
-                  alt="Generic placeholder image"
-                />
-              </Media>
-              <Media body>
-                <Media heading>
-                  <strong>War</strong>
+
+              <hr />
+            </FadeTransform>
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+              delay="3000"
+            >
+              <Media>
+                <Media left className="my-auto">
+                  <Media
+                    object
+                    className="rules-card mr-4"
+                    src="../cards/backcard.jpg"
+                    alt="Generic placeholder image"
+                  />
                 </Media>
-                <p>
-                  In the event both cards have equal value, you must go to WAR.
-                  In Quadruple War, each player stakes 4 cards face-down from
-                  their hand and turns the fifth card face-up. The highest value
-                  card is the winner of the WAR and places all the cards won at
-                  the bottom of their player deck.
-                </p>
-              </Media>
-            </Media>
-            <hr />
-            <Media>
-              <Media left className="my-auto">
-                <Media
-                  object
-                  className="rules-card mr-4"
-                  src="../cards/backcard.jpg"
-                  alt="Generic placeholder image"
-                />
-              </Media>
-              <Media body>
-                <Media heading>
-                  <strong>If you can't go to WAR</strong>
+                <Media body>
+                  <Media heading>
+                    <strong>Game Play</strong>
+                  </Media>
+                  <p>
+                    Each player takes the top card of their hand and flips it
+                    face-up. The player with the highest value card, wins the
+                    hand. The card values are in numerical order from 2 to 14.
+                    Number cards reflect their values(2-10), Jacks are worth 11,
+                    Queens 12, Kings 13, and Aces high at 14. The winning player
+                    takes both cards and places them at the bottom of their
+                    player deck.
+                  </p>
                 </Media>
-                <p>
-                  If you have less than 5 cards in your player deck, you don't
-                  have enough cards to stake the WAR and turn over a card. I'm
-                  sorry my friend, your final battle has been fought. You lose!
-                </p>
               </Media>
-            </Media>
-            <hr />
-            <Media>
-              <Media left className="my-auto">
-                <Media
-                  object
-                  className="rules-card mr-4"
-                  src="../cards/backcard.jpg"
-                  alt="Generic placeholder image"
-                />
-              </Media>
-              <Media body>
-                <Media heading>
-                  <strong>
-                    If at any point you have no cards to play in your player
-                    deck, You Lose!
-                  </strong>
+              <hr />
+            </FadeTransform>
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+              delay="4000"
+            >
+              <Media>
+                <Media left className="my-auto">
+                  <Media
+                    object
+                    className="rules-card mr-4"
+                    src="../cards/backcard.jpg"
+                    alt="Generic placeholder image"
+                  />
                 </Media>
-                <p>You have been bested by a worthy opponent!</p>
+                <Media body>
+                  <Media heading>
+                    <strong>War</strong>
+                  </Media>
+                  <p>
+                    In the event both cards have equal value, you must go to
+                    WAR. In Quadruple War, each player stakes 4 cards face-down
+                    from their hand and turns the fifth card face-up. The
+                    highest value card is the winner of the WAR and places all
+                    the cards won at the bottom of their player deck.
+                  </p>
+                </Media>
               </Media>
-            </Media>
+              <hr />
+            </FadeTransform>
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+              delay="5000"
+            >
+              <Media>
+                <Media left className="my-auto">
+                  <Media
+                    object
+                    className="rules-card mr-4"
+                    src="../cards/backcard.jpg"
+                    alt="Generic placeholder image"
+                  />
+                </Media>
+                <Media body>
+                  <Media heading>
+                    <strong>If you can't go to WAR</strong>
+                  </Media>
+                  <p>
+                    If you have less than 5 cards in your player deck, you don't
+                    have enough cards to stake the WAR and turn over a card. I'm
+                    sorry my friend, your final battle has been fought. You
+                    lose!
+                  </p>
+                </Media>
+              </Media>
+              <hr />
+            </FadeTransform>
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateX(-100%)",
+              }}
+              delay="6000"
+            >
+              <Media>
+                <Media left className="my-auto">
+                  <Media
+                    object
+                    className="rules-card mr-4"
+                    src="../cards/backcard.jpg"
+                    alt="Generic placeholder image"
+                  />
+                </Media>
+                <Media body>
+                  <Media heading>
+                    <strong>
+                      If at any point you have no cards to play in your player
+                      deck, You Lose!
+                    </strong>
+                  </Media>
+                  <p>You have been bested by a worthy opponent!</p>
+                </Media>
+              </Media>
+            </FadeTransform>
           </div>
         </div>
       </div>
